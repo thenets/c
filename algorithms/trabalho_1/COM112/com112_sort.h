146e90bbfba92599dc70a90198219bff1391cb57
Luiz Costa <phelip@thenets.org>
2018-04-20 17:47:12 -0300

    Old com112_trabalho_1

---
+++ algorithms/trabalho_1/COM112/com112_sort.h
@@ -0,0 +1,8 @@
+#ifndef SORT_H   // guardas de cabeçalho
+#define SORT_H
+
+
+void bubbleSort(int *V, int n, int *n_comp, int *n_mov);
+void insertionSort(int *V, int n, int *n_comp, int *n_mov);
+
+#endif
